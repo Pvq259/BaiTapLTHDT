4578effefcae70ffb435481ce979d3c39baa6e34
Pvq259 <pvq259207@gmail.com>
2026-06-17 21:04:05 +0700

    Thêm vào số lượng sản phẩm

--- SanPham.cpp
+++ SanPham.cpp
@@ -1,56 +1,84 @@
 #include "SanPham.h"
+#include <iomanip>
 
-SanPham::SanPham(){
+using namespace std;
+
+// Hàm mặc định
+SanPham::SanPham()
+{
     maSP = "";
     tenSP = "";
     gia = 0;
+    soLuong = 0;
 }
 
-SanPham::SanPham(string ma, string ten, double gia){
+// Hàm mặc định có tham số
+SanPham::SanPham(string ma, string ten, double gia, int soLuong)
+{
     this->maSP = ma;
     this->tenSP = ten;
     this->gia = gia;
+    this->soLuong = soLuong;
 }
 
-string SanPham::getMaSP() const{
+// Getter
+string SanPham::getMaSP() const
+{
     return maSP;
 }
-
-string SanPham::getTenSP() const{
+string SanPham::getTenSP() const
+{
     return tenSP;
 }
-
-double SanPham::getGia() const{
+double SanPham::getGia() const
+{
     return gia;
 }
+int SanPham::getSoLuong() const
+{
+    return soLuong;
+}
 
-void SanPham::setMaSP(string ma){
+// Setter
+void SanPham::setMaSP(string ma)
+{
     maSP = ma;
 }
-
-void SanPham::setTenSP(string ten){
+void SanPham::setTenSP(string ten)
+{
     tenSP = ten;
 }
-
-void SanPham::setGia(double gia){
+void SanPham::setGia(double gia)
+{
     this->gia = gia;
 }
-
-void SanPham::nhap(){
-    cout << "Ma san pham: ";
-    getline(cin, maSP);
-
-    cout << "Ten san pham: ";
-    getline(cin, tenSP);
-
-    cout << "Gia: ";
-    cin >> gia;
-    cin.ignore();
+void SanPham::setSoLuong(int soLuong)
+{
+    this->soLuong = soLuong;
 }
 
-void SanPham::xuat() const {
-    cout << "Ma San Pham : "<< maSP <<endl;
-    cout << "Ten San Pham : " << tenSP << endl;
-    cout << "Gia San Pham : "<< gia << endl;
+// Nhập thông tin sản phẩm
+void SanPham::nhap()
+{
+    cin.ignore();
+    cout << "Nhap ma san pham: ";
+    getline(cin, maSP);
+    cout << "Nhap ten san pham: ";
+    getline(cin, tenSP);
+    cout << "Nhap gia: ";
+    cin >> gia;
+    cout << "Nhap so luong: ";
+    cin >> soLuong;
+}
+
+// Xuất thông tin sản phẩm
+void SanPham::xuat() const
+{
+    cout << left
+         << setw(15) << maSP
+         << setw(30) << tenSP
+         << setw(15) << fixed << setprecision(2) << gia      //Làm tròn giá sp dến 2 số thập phân 
+         << setw(10) << soLuong
+         << endl;
 }
 
--- SanPham.h
+++ SanPham.h
@@ -12,19 +12,25 @@ private:
     string maSP;
     string tenSP;
     double gia;
+    int soLuong;
 
 public:
     SanPham();
-    SanPham(string ma, string ten, double gia);
+    SanPham(string ma, string ten, double gia, int soLuong);
 
+    // Getter
     string getMaSP() const;
     string getTenSP() const;
     double getGia() const;
+    int getSoLuong() const;
 
+    // Setter
     void setMaSP(string ma);
     void setTenSP(string ten);
     void setGia(double gia);
+    void setSoLuong(int soLuong);
 
+    // Nhập xuất
     void nhap();
     void xuat() const;
 };
